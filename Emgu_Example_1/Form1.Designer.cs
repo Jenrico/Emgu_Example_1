bb82ea2212ecf4eaaf5051ecd66912447a7a1009
jenrico <jenrico@MP05WCQQ>
2018-04-30 16:08:17 -0400

    Add project files.

---
+++ Emgu_Example_1/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Emgu_Example_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(666, 496);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(735, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 94);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(735, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(735, 159);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(861, 491);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(735, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "N/A";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(833, 428);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "N/A";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(735, 490);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "N/A";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(735, 367);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "N/A";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 520);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Binary Form";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Emgu_Example_1/Form1.cs
@@ -0,0 +1,202 @@
+﻿//Final Code
+//Jacob Enrico
+//Started: 4-18-18
+//Finished: 4-27-18
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using System.Threading;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.IO.Ports;
+using System.Diagnostics;
+
+namespace Emgu_Example_1
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread _captureThread;
+        int X, Y;
+        int d = 20; // Divider
+
+        SerialPort _serialPort = new SerialPort("COM4", 2400); // Connection to L2 Bot
+
+        // COMMANDS //
+        const byte STOP = 0x7F;
+        const byte FLOAT = 0x0F;
+        const byte FORWARD = 0x6F;
+        const byte BACKWARD = 0x5F;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture();
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start();
+
+            _serialPort.DataBits = 8;
+            _serialPort.Parity = Parity.None;
+            _serialPort.StopBits = StopBits.Two;
+            _serialPort.Open();
+        }
+
+        private void DisplayWebcam()
+        {
+            while (_capture.IsOpened)
+            {
+                Mat frame = _capture.QueryFrame();
+                CvInvoke.Resize(frame, frame, pictureBox1.Size);
+                Image<Gray, Byte> grayImage = frame.ToImage<Gray, Byte>();
+                Image<Hsv, Byte> red = frame.ToImage<Bgr, Byte>().Convert<Hsv, Byte>();
+                Image<Gray, Byte>[] redCheck = red.Split();
+
+                redCheck[2] = redCheck[1].Clone().ThresholdBinary(new Gray(65), new Gray(255));
+
+                grayImage = grayImage.ThresholdBinary(new Gray(X), new Gray(Y));
+
+                int leftWhiteCount = 0; // Counting left side white
+                for (int xx = 0; xx < (grayImage.Width * 1 / 3); xx++)
+                {
+                    for (int yy = 0; yy < grayImage.Height * 1 / 2; yy++)
+                    {
+                        if (grayImage.Data[yy, xx, 0] == Y)
+                            leftWhiteCount++;
+                    }
+                }
+
+                int rightWhiteCount = 0; // Counting right side white
+                for (int xxx = (grayImage.Width * 2 / 3); xxx < grayImage.Width; xxx++)
+                {
+                    for (int yyy = 0; yyy < grayImage.Height * 1 / 2; yyy++)
+                    {
+                        if (grayImage.Data[yyy, xxx, 0] == Y)
+                            rightWhiteCount++;
+                    }
+                }
+
+                int redCount = 0; // Counting red in middle
+                for (int xxxx = (grayImage.Width * 1 / 3); xxxx < (grayImage.Width * 2 / 3); xxxx++)
+                {
+                    for (int yyyy = 0; yyyy < grayImage.Height * 1 / 2; yyyy++)
+                    {
+                        if (redCheck[2].Data[yyyy, xxxx, 0] == 255)
+                            redCount++;
+                    }
+                }
+
+                // PIXEL COUNT DISPLAYS //
+                String LWC = leftWhiteCount.ToString();
+                label2.Invoke(new Action(() => label2.Text = LWC));
+
+                String RWC = rightWhiteCount.ToString();
+                label3.Invoke(new Action(() => label3.Text = RWC));
+
+                String RC = redCount.ToString();
+                label5.Invoke(new Action(() => label5.Text = RC));
+
+                if (redCount > (((grayImage.Width * 1 / 3) * (grayImage.Height * 1 / 2)) * 6.5  / d)) // Looking for red
+                {
+                    label4.Invoke(new Action(() => label4.Text = "Turn Around"));
+
+                    byte[] buffer = { 0x01, BACKWARD, FORWARD };
+                    _serialPort.Write(buffer, 0, 3);
+
+                    Thread.Sleep(1400);
+                }
+
+                else // Running commands
+                {
+                    if ((leftWhiteCount & rightWhiteCount) >= (((grayImage.Width * 1 / 3) * (grayImage.Height * 1 / 2)) * 1 / d))
+                    {
+                        label4.Invoke(new Action(() => label4.Text = "Move Forward"));
+
+                        byte[] buffer = { 0x01, FORWARD, FORWARD };
+                        _serialPort.Write(buffer, 0, 3);
+                    }
+
+                    else if (rightWhiteCount < (((grayImage.Width * 1 / 3) * (grayImage.Height * 1 / 2)) * 1 / (1.5 * d)))
+                    {
+                        label4.Invoke(new Action(() => label4.Text = "Move Left"));
+
+                        byte[] buffer = { 0x01, STOP, FORWARD };
+                        _serialPort.Write(buffer, 0, 3);
+                    }
+
+                    else if (leftWhiteCount < (((grayImage.Width * 1 / 3) * (grayImage.Height * 1 / 2)) * 1 / (1.5 * d)))
+                    {
+                        label4.Invoke(new Action(() => label4.Text = "Move Right"));
+
+                        byte[] buffer = { 0x01, FORWARD, STOP };
+                        _serialPort.Write(buffer, 0, 3);
+                    }
+                }
+                //pictureBox1.Image = redCheck[2].ToBitmap(); // For my testing
+                pictureBox1.Image = grayImage.Bitmap; // Shows the black and white image
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _captureThread.Abort();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Inputs for "new Gray"
+        {
+            String x = textBox1.Text;
+            String y = textBox2.Text;
+
+            X = int.Parse(x);
+            Y = int.Parse(y);
+
+            button1.BackColor = Color.CornflowerBlue;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
